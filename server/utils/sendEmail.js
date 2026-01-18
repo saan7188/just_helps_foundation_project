901d2c6edd372abfab50f4092764b08ca859b044
Saan <ssksanjay66@gmail.com>
2026-01-18 18:07:57 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,8 +5,8 @@ const sendEmail = async (to, subject, text, html = null) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // MUST be false for 587
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // App Password
