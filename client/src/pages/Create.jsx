fac1b7cef116b65d632622af834816994c9093f2
Saan <ssksanjay66@gmail.com>
2026-09-20 11:51:22 +0530

    Use centralized API configuration

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = "https://justhelpsserver.onrender.com";
+import API_URL from '../api';
 
 const Create = () => {
   const navigate = useNavigate();
